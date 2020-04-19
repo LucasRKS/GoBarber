312adb2e2e65994fdd62dbd0d6c3d452adf41316
Lucas Karger <lucasrogeriosk@gmail.com>
2020-04-19 17:34:22 -0300

    Interface e validação de data

--- Primero Projeto Node/src/routes/appointments.routes.ts
+++ Primero Projeto Node/src/routes/appointments.routes.ts
@@ -1,9 +1,41 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfDay, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (req, res) => {
+    const { provider, date } = req.body;
+
+    const parsedDate = startOfDay(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate) {
+        return res
+            .status(400)
+            .json({ message: 'This appointment is alredy booked.' });
+    }
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return res.json(appointment);
 });
 
 export default appointmentsRouter;
