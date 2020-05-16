d009c5cf93410e8ff36c6437287160230ee56979
Lucas Karger <lucasrogeriosk@gmail.com>
2020-05-16 12:02:24 -0300

    Autenticação jwt, rotas autenticadas

---
+++ GoBarber backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '88edcf618a409c1bf70ae25e264bcc5e', // bananabarber
+        expiresIn: '1d',
+    },
+};
--- GoBarber backend/src/routes/index.ts
+++ GoBarber backend/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import AppointmentsRouter from './appointments.routes';
 import UsersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
+routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', AppointmentsRouter);
 routes.use('/users', UsersRouter);
 
