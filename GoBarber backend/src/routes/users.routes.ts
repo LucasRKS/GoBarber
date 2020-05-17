68fc2044ed1d2f51bf93806fee335778b4d6279d
Lucas Karger <lucasrogeriosk@gmail.com>
2020-05-17 13:58:25 -0300

    Tratando arquivos e imagem de perfil user

--- GoBarber backend/src/routes/users.routes.ts
+++ GoBarber backend/src/routes/users.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (req, res) => {
     try {
@@ -19,5 +26,25 @@ userRouter.post('/', async (req, res) => {
         return res.status(400).json({ message: err.message });
     }
 });
+
+userRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        try {
+            const updateUserAvatarService = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatarService.execute({
+                user_id: req.user.id,
+                avatarFilename: req.file.filename,
+            });
+
+            return res.json(user);
+        } catch (err) {
+            return res.status(400).json({ message: err.message });
+        }
+    },
+);
 
 export default userRouter;
