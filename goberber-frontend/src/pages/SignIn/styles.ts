3cd3055b73f562ca1f96cd951cdc6e18eb64f46a
Lucas Karger <lucasrogeriosk@gmail.com>
2020-06-03 18:07:44 -0300

    Configurando eslint e estilizando login

---
+++ goberber-frontend/src/pages/SignIn/styles.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import backgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      color: #FFF;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+
+      & + input {
+        margin-top: 8px
+      }
+    }
+
+    button {
+      background: #FF9000;
+      border-radius: 10px;
+      border: 0;
+      padding: 16px;
+      color: #321e38;
+      width: 100%;
+      font-weight: 500;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#FF9000')}
+      }
+    }
+
+    a {
+      color: #F4edeB;
+      text-decoration: none;
+      display: block;
+      margin-top: 24px;
+
+      &:hover {
+        color: ${shade(0.2, '#F4edeB')}
+      }
+    }
+  }
+
+  > a {
+    color: #FF9000;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    margin-top: 24px;
+
+    &:hover {
+      color: ${shade(0.2, '#FF9000')}
+    }
+
+    svg {
+      margin-right: 0 15px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center;
+  background-size: cover;
+`;
