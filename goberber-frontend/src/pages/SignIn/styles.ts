27418994614ee501d9d0beb8be994b86e467d90a
Lucas Karger <lucasrogeriosk@gmail.com>
2020-06-05 11:05:55 -0300

    Reconfigurando prettier e eslint

--- goberber-frontend/src/pages/SignIn/styles.ts
+++ goberber-frontend/src/pages/SignIn/styles.ts
@@ -28,19 +28,19 @@ export const Content = styled.div`
 
     input {
       background: #232129;
-      color: #FFF;
+      color: #fff;
       border-radius: 10px;
       border: 2px solid #232129;
       padding: 16px;
       width: 100%;
 
       & + input {
-        margin-top: 8px
+        margin-top: 8px;
       }
     }
 
     button {
-      background: #FF9000;
+      background: #ff9000;
       border-radius: 10px;
       border: 0;
       padding: 16px;
@@ -51,30 +51,30 @@ export const Content = styled.div`
       margin-top: 16px;
 
       &:hover {
-        background: ${shade(0.2, '#FF9000')}
+        background: ${shade(0.2, '#FF9000')};
       }
     }
 
     a {
-      color: #F4edeB;
+      color: #f4edeb;
       text-decoration: none;
       display: block;
       margin-top: 24px;
 
       &:hover {
-        color: ${shade(0.2, '#F4edeB')}
+        color: ${shade(0.2, '#F4edeB')};
       }
     }
   }
 
   > a {
-    color: #FF9000;
+    color: #ff9000;
     text-decoration: none;
     display: flex;
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#FF9000')}
+      color: ${shade(0.2, '#FF9000')};
     }
 
     svg {
