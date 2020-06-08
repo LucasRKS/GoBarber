2e0500dbc9f2ec04d1771dac344404fd3761369b
Lucas Karger <lucasrogeriosk@gmail.com>
2020-06-08 18:31:26 -0300

    Exibindo erros no input

--- goberber-frontend/src/pages/SignUp/index.tsx
+++ goberber-frontend/src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
 import { FiArrowLeft, FiUser, FiLock, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import getValidationErrros from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
@@ -11,11 +14,17 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: any) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('O nome é obrigatório.'),
-        email: Yup.string().required().email('Insira um e-mail válido.'),
+        email: Yup.string()
+          .required('O e-mail é obrigatório')
+          .email('Insira um e-mail válido.'),
         password: Yup.string().min(6, 'A senha deve ter no mínimo 6 dígitos.'),
       });
 
@@ -23,7 +32,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (e) {
-      console.log(e);
+      const errors = getValidationErrros(e);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -34,7 +45,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Informe seu Nome" />
