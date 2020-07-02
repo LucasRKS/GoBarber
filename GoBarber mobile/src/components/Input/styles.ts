a48f489d26be8aae4362c7f4525a93717d299828
Lucas Karger <lucasrogeriosk@gmail.com>
2020-07-02 10:49:36 -0300

    Corrigindo css botão e input

--- GoBarber mobile/src/components/Input/styles.ts
+++ GoBarber mobile/src/components/Input/styles.ts
@@ -26,7 +26,7 @@ export const Container = styled.View<ContainerProps>`
 `;
 
 export const TextInput = styled.TextInput`
-  flex: 1%;
+  flex: 1;
   color: #fff;
   font-size: 16px;
   font-family: 'RobotoSlab-Regular';
